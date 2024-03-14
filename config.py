b8c76c33838e56cccf6eb5a6eae10ab0d66f8adc
Gavino Richner <gavino@Mate.local>
2024-03-14 17:08:10 +0100

    Version 1.0

---
+++ config.py
@@ -0,0 +1,17 @@
+#tutorial/config.py
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'erraetst-Du-nie'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SEVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
+    ADMINS = ['admin@does.not.exist.com']
+    POSTS_PER_PAGE = 25
